36b9e9c95e5c61c377f3ab17839fe80e40e36b65
firozt <firozt03@gmail.com>
2024-11-23 13:36:40 +0000

    init client

---
+++ client/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{html,jsx,tsx}',
+    // you can either add all styles
+    './node_modules/@rewind-ui/core/dist/theme/styles/*.js',
+    // OR you can add only the styles you need
+    // './node_modules/@rewind-ui/core/dist/theme/styles/Button.styles.js',
+    // './node_modules/@rewind-ui/core/dist/theme/styles/Text.styles.js'
+  ],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('tailwind-scrollbar')({ nocompatible: true }),
+    require('@tailwindcss/forms')({
+      strategy: 'class' // only generate classes
+    })
+  ]
+};
